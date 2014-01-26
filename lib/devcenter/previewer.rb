1248d111df8a2db6ce8e00f4a02706ea64079db2
Raul Murciano <raul@murciano.net>
2014-01-26 15:20:58 +0100

    Cleanup Previewer

--- lib/devcenter/previewer.rb
+++ lib/devcenter/previewer.rb
@@ -4,38 +4,69 @@ require_relative 'previewer/web_server'
 require_relative 'previewer/file_listener'
 
 module Devcenter::Previewer
-
   extend self
 
   def preview(slug, md_path, host, port)
-    server = WebServer.new(host, port, WebApp)
-    file_listener_callback = Proc.new do |modified, added, removed|
-      modified.each{ |f| Devcenter::Logger.log "File modified: #{f}" }
-      WebApp.send_server_event
-    end
-
-    listener = FileListener.new(md_path, file_listener_callback)
-
-    server.start
-    listener.start
-
-    url = "http://#{host}:#{port}/#{slug}"
-    say "\nLive preview for #{slug} available in #{url}"
-    say "It will refresh when you save #{md_path}"
-    say "Press Ctrl+C to exit...\n"
-    Launchy.open(url)
-
-    running = true
-    [:INT, :TERM].each do |signal|
-      trap(signal) do
-        listener.stop
-        server.stop
-        running = false
-        say "\nPreview finished."
-        exit
-      end
-    end
-    sleep 1 while running
+    Runner.new(slug, md_path, host, port).run
   end
 
+  class Runner
+    attr_reader :slug, :md_path, :host, :port, :url
+
+    def initialize(slug, md_path, host, port)
+      @slug = slug
+      @md_path = md_path
+      @host = host
+      @port = port
+      @url = "http://#{@host}:#{@port}/#{@slug}"
+      @listener = init_listener
+      @server = init_server
+    end
+
+    def run
+      start
+      @running = true
+      [:INT, :TERM].each do |signal|
+        trap(signal) do
+          stop
+          exit
+        end
+      end
+      sleep 1 while running?
+    end
+
+    private
+
+    def running?
+      @running
+    end
+
+    def init_server
+      WebServer.new(host, port, WebApp)
+    end
+
+    def init_listener
+      file_listener_callback = Proc.new do |modified, added, removed|
+        modified.each{ |f| Devcenter::Logger.log "File modified: #{f}" }
+        WebApp.send_server_event
+      end
+      FileListener.new(md_path, file_listener_callback)
+    end
+
+    def start
+      @server.start
+      @listener.start
+      say "\nLive preview for #{slug} available in #{url}"
+      say "It will refresh when you save #{md_path}"
+      say "Press Ctrl+C to exit...\n"
+      Launchy.open(url)
+    end
+
+    def stop
+      @server.stop
+      @listener.stop
+      say "\nPreview finished."
+      @running = false
+    end
+  end
 end
