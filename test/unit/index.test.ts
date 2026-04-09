917153fcbea4b4e5e269c29af7f4704bf23cb43e
Michael Malave <michael.malave@salesforce.com>
2026-04-09 13:50:35 -0700

    updating test coverage in this PR

--- test/unit/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai'
-
-import {name} from '../../src/index.js'
-
-describe('package entry', function () {
-  it('exports the npm package name', function () {
-    expect(name).to.equal('@heroku-cli/heroku-cli-plugin-devcenter')
-  })
-})
